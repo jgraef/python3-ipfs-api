feebb2731bd428539582b6aa874c8f5e707b99a2
Janosch Gräf <graef@st.cs.uni-saarland.de>
2016-01-12 00:33:06 +0100

    docstrings for ipfs.api

--- ipfs/api/__init__.py
+++ ipfs/api/__init__.py
@@ -24,19 +24,54 @@ class IpfsApi:
         self.file = FileApi(r)
 
 
-    def id(self):
-        return self._rpc.id.with_outputenc(codec.JSON)()
+    def id(self, peer_id = None):
+        """
+        Return information about the specified IPFS peer. If no peer is
+        specified, own peer information will be returned.
+
+        ``peer_id``: Peer ID of node to look up (optional)
+
+        returns: A dict with:
+                   ``ID``:              The peer's ID
+                   ``PublicKey``:       The peer's public key encoded as base64
+                   ``Addresses``:       A list of the peer's addresses,
+                   ``AgentVersion``:    The peer's agent version
+                   ``ProtocolVersion``: The peer's protocol version
+                   
+        """
+
+        args = (peer_id,) if peer_id else ()
+        return self._rpc.id.with_outputenc(codec.JSON)(*args)
 
 
     def version(self):
+        """
+        Return IPFS version information.
+
+        returns: A dict with:
+                   ``Version``: Version number,
+                   ``Commit``:  Commit hash,
+                   ``Repo``:    Repo version
+        """
+
         return self._rpc.version.with_outputenc(codec.JSON)()
 
 
     def resolve(self, ref, recursive = True):
+        """
+        Resolve a name.
+
+        ``name``:      The name to resolve
+        ``recursive``: Resolve until the name is an IPFS name
+
+        returns: The resolved IPFS name
+        """
+        
         return self._rpc.resolve.with_outputenc(codec.JSON)(ref, recursive = recursive)
 
 
     def repo_gc(self):
+        """ Perform a garbage collection sweep on the repo. """
         return self._rpc.repo.gc.with_outputenc(codec.JSONV)()
 
 
