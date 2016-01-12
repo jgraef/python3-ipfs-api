4af2f2d88e538534e8749f7c561fb525573fc8d3
Janosch Gräf <graef@st.cs.uni-saarland.de>
2016-01-12 22:38:29 +0100

    updated long description

--- setup.py
+++ setup.py
@@ -19,21 +19,16 @@ setup(name = "python3-ipfs-api",
       license = "MIT",
 
       long_description = """
-This is a complete rewrite of python-ipfs-api. It defines a whole new interface
-and tries to do more than just wrapping the HTTP API. python3-ipfs-api will
-only support Python 3 (hence the name).
+python3-ipfs-api is a complete rewrite of [python-ipfs-api](https://github.com/ipfs/python-ipfs-api).
+It defines a whole new interface and tries to do more than just wrapping the
+HTTP API. python3-ipfs-api will only support Python 3 (hence the name).
 
-python3-ipfs-api uses requests to issue API calls. Also it has a minimal
-implementation of a protobuf reader/writer for using protobuf encoding where
-possible. This makes it possible to e.g. put/get objects with binary content
-without a problem.
+python3-ipfs-api uses [requests](http://python-requests.org) to issue API
+calls. Also it has a minimal implementation of a protobuf reader/writer for
+using protobuf encoding where possible. This makes it possible to e.g. put/get
+objects with binary content without a problem.
 
-In ipfs.api you'll find the lowlevel API implementation. Block, DHT and parts
-of Object commands already work.
-
-There will be high-level APIs. Currently only ipfs.merkledag is available. An
+In ipfs.api you'll find the lowlevel API implementation. The high-level API is
+still work in progess and only consists of the ifps.merkledag module at the moment. An
 API for interfacing unixfs in a pythonic way will come soon.
-
-This software is still work in progress. Use the original python-ipfs-api
-(https://github.com/ipfs/python-ipfs-api) where possible.
 """)
